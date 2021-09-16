d7dedf54a34d376045d422df67a359407e42a185
Duc Tran <victor.tran.august@gmail.com>
2021-09-16 11:47:28 +0700

    add redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
 import "./styles.scss";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </StrictMode>,
   document.getElementById("root")
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,45 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['auth']
+}
 import authSlice from './authSlice';
 import counterSlice from './counterSlice'
 import todoSlice from './todoSlice';
 
+const persistedReducer = persistReducer(persistConfig, combineReducers({
+  counter: counterSlice,
+  todo: todoSlice,
+  auth: authSlice
+}))
+
 const store = configureStore({
-  reducer: {
-    counter: counterSlice,
-    todo: todoSlice,
-    auth: authSlice
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
 
+export const persistor = persistStore(store)
+
 export type RootState = ReturnType<typeof store.getState>
+
 
 export default store;
